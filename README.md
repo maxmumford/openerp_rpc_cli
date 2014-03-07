7b1eba38edcd39adb4f709f85aa2fc335380df8c
Max Mumford <maxmumford@gmail.com>
2014-03-07 16:48:04 +0100

    [ADD] openerp_rpc_cli class v1 [ADD] documentation to readme

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 openerp-rpc-cli
 ===============
 
-Provides argparse arguments for classes that need an OpenERP RPC connection, without you having to retype them every... single... time!
+A simple wrapper around openerp-client-lib that provides you with various argparse CLI arguments so you don't have to re-type them each time you write a script that needs an OpenERP RPC connection.
+
+Usage
+-----
+
+Simple usage example:
+
+```
+from openerp_rpc_cli import openerp_rpc_cli
+
+class my_oe_cli(openerp_rpc_cli):
+
+	description = "Description shown in argparse help message"
+
+	def set_arguments(self, parser):
+		""" Manually add parameters to the argparse object """
+		parser.add_argument('my_awsome_parameter', type=str)
+
+	def do(self, arguments, connection):
+		""" Do some stuff! """
+		pass # but I am le tired!
+
+my_oe_cli()
+```
+
+In the above example we add an additional CLI argument to the existing OE RPC ones specified by the openerp_rpc_cli class. Then we put the bulk of our actual code in the do function, making use of the arguments and connection properties. Don't forget to instantiate your class afterwards! do() will be called automatically in the __init__ function of the openerp-rpc-cli class.
---
+++ openerp_rpc_cli.py
@@ -0,0 +1,65 @@
+import argparse
+import openerplib
+
+class openerp_rpc_cli(object):
+	"""
+	Wrapper around openerp-client-lib (openerplib) to provide argparse command line
+	arguments to CLI programs that use an OE RPC connections.
+
+	To use, inherit this class and override the do(arguments,  connection) function.
+	You can also override the set_arguments(parser) function to set your own argparse
+	arguments. self.description provides the argparse CLI description. For more info 
+	see example.py file.
+	"""
+
+	description = """
+	YOUR AD HERE! Override the description attribute in your child class to change this message.
+	"""
+
+	def __init__(self):
+		"""
+		Setup RPC cli arguments 
+		"""
+		# setup argparse RPC arguments
+		self.parser = argparse.ArgumentParser(self.description)
+
+		self.parser.add_argument('database', type=str, help='Name of database to connect to')
+		self.parser.add_argument('--hostname', type=str, help='Hostname of the OpenERP server', default='localhost')
+		self.parser.add_argument('--port', type=int, help='RPC port of the OpenERP server', default=0)
+		self.parser.add_argument('--protocol', type=str, help='The protocol used by the RPC connection', default='xmlrpc')
+		self.parser.add_argument('--username', type=str, help='Username to connect to the OpenERP server', default='admin')
+		self.parser.add_argument('--password', type=str, help='Password to connect to the OpenERP server', default='admin')
+		self.parser.add_argument('--saas', type=bool, help="If connecting to SaaS, automatically set appropriate port and protocol", default=False)
+
+		# call set_arguments to let user define their own cli arguments
+		self.set_arguments(self.parser)
+
+		# parse args
+		self.args = self.parser.parse_args()
+		
+		# The saas option sets up some params to be used with OE SaaS instances
+		if self.args.saas:
+			self.args.protocol = 'xmlrpcs'
+			self.args.port = 443
+
+		# Open the connection and check the login
+		self.conn = openerplib.get_connection(hostname=self.args.hostname, database=self.args.database, login=self.args.username, password=self.args.password, port=self.args.port or 'auto', protocol=self.args.protocol)
+		self.conn.check_login()
+
+		# Call self.do to run the users code
+		self.do(self.args, self.conn)
+
+	def set_arguments(self, parser):
+		"""
+		Override me and use parser to manually set your own CLI arguments. No need to call super.
+		@param parser: instance of argparse.ArgumentParser
+		"""
+		pass
+
+	def do(self, arguments, connection):
+		"""
+		Override me and do something with arguments and connection! No need to call super.
+		@param arguments: argparse arguments as returned by argparse.ArgumentParser().parse_args()
+		@param connection: openerplib RPC connection
+		"""
+		pass
